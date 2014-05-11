5b5c25398a9b7d18c06c479b57a3b11c1f30b720
Artur Balakin <arturunknown@gmx.net>
2014-05-11 20:39:09 +0200

    let miner thread run in high priority

--- cgminer.c
+++ cgminer.c
@@ -4916,6 +4916,18 @@ void zero_stats(void)
 	}
 }
 
+static void set_highprio(void)
+{
+#ifndef WIN32
+	int ret = nice(-10);
+
+	if (!ret)
+		applog(LOG_DEBUG, "Unable to set thread to high priority");
+#else
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+#endif
+}
+
 static void set_lowprio(void)
 {
 #ifndef WIN32
@@ -7322,6 +7334,7 @@ void *miner_thread(void *userdata)
 	applog(LOG_DEBUG, "Waiting on sem in miner thread");
 	cgsem_wait(&mythr->sem);
 
+	set_highprio();
 	cgpu->last_device_valid_work = time(NULL);
 	drv->hash_work(mythr);
 out:
